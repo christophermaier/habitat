e866bd45410aa9c4f213cc9a858a82b45bc43c17
Christopher Maier <cmaier@chef.io>
2018-01-19 15:29:42 -0500

    WIP: run hooks as correct user
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup/src/sys/unix/exec.rs
+++ components/sup/src/sys/unix/exec.rs
@@ -16,6 +16,8 @@ use std::ffi::OsStr;
 use std::os::unix::process::CommandExt;
 use std::process::{Child, Command, Stdio};
 
+use sys::abilities;
+
 use hcore::os;
 
 use error::{Error, Result};
@@ -29,20 +31,42 @@ where
     S: AsRef<OsStr>,
 {
     let mut cmd = Command::new(path);
-    let uid = os::users::get_uid_by_name(&pkg.svc_user).ok_or(sup_error!(
-        Error::Permissions(format!(
-            "No uid for user '{}' could be found",
-            &pkg.svc_user
-        ))
-    ))?;
-    let gid = os::users::get_gid_by_name(&pkg.svc_group).ok_or(
-        sup_error!(
+
+    // TODO (CM): these need to be the UID/GID that we're running as,
+    // not, the svc user / group....
+
+    //
+    //
+    // Ugh, this is exactly the same logic that the launcher has...
+
+    let uid;
+    let gid;
+
+    // TODO (CM): if we can set the process and group, then let's only
+    // call uid() and gid() in that block; set up the cmd beforehand
+
+
+
+    if abilities::can_set_process_user_and_group() {
+        uid = os::users::get_uid_by_name(&pkg.svc_user).ok_or(sup_error!(
             Error::Permissions(format!(
-                "No gid for group '{}' could be found",
-                &pkg.svc_group
+                "No uid for user '{}' could be found",
+                &pkg.svc_user
             ))
-        ),
-    )?;
+        ))?;
+        gid = os::users::get_gid_by_name(&pkg.svc_group).ok_or(
+            sup_error!(
+                Error::Permissions(format!(
+                    "No gid for group '{}' could be found",
+                    &pkg.svc_group
+                ))
+            ),
+        )?;
+    } else {
+        uid = os::users::get_effective_uid();
+        gid = os::users::get_effective_gid().unwrap(); // // TODO (CM):
+        // I know, it's bad
+    }
     cmd.stdin(Stdio::null())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -51,5 +75,9 @@ where
     for (key, val) in pkg.env.iter() {
         cmd.env(key, val);
     }
-    Ok(cmd.spawn()?)
+
+    let res = cmd.spawn();
+    //    debug!(">>>>>>> res = {:?}", res);
+
+    Ok(res?)
 }

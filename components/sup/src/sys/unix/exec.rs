6edceb6b53784dee1d78e3f53b8595b4cc6d141a
Christopher Maier <cmaier@chef.io>
2018-01-19 17:37:54 -0500

    Clean up exec
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup/src/sys/unix/exec.rs
+++ components/sup/src/sys/unix/exec.rs
@@ -31,30 +31,24 @@ where
     S: AsRef<OsStr>,
 {
     let mut cmd = Command::new(path);
-
-    // TODO (CM): these need to be the UID/GID that we're running as,
-    // not, the svc user / group....
-
-    //
-    //
-    // Ugh, this is exactly the same logic that the launcher has...
-
-    let uid;
-    let gid;
-
-    // TODO (CM): if we can set the process and group, then let's only
-    // call uid() and gid() in that block; set up the cmd beforehand
-
-
+    cmd.stdin(Stdio::null()).stdout(Stdio::piped()).stderr(
+        Stdio::piped(),
+    );
+    for (key, val) in pkg.env.iter() {
+        cmd.env(key, val);
+    }
 
     if abilities::can_set_process_user_and_group() {
-        uid = os::users::get_uid_by_name(&pkg.svc_user).ok_or(sup_error!(
+        // If we can SETUID/SETGID, then run the script as the service
+        // user; otherwise, we'll just run it as ourselves.
+
+        let uid = os::users::get_uid_by_name(&pkg.svc_user).ok_or(sup_error!(
             Error::Permissions(format!(
                 "No uid for user '{}' could be found",
                 &pkg.svc_user
             ))
         ))?;
-        gid = os::users::get_gid_by_name(&pkg.svc_group).ok_or(
+        let gid = os::users::get_gid_by_name(&pkg.svc_group).ok_or(
             sup_error!(
                 Error::Permissions(format!(
                     "No gid for group '{}' could be found",
@@ -62,22 +56,10 @@ where
                 ))
             ),
         )?;
+        cmd.uid(uid).gid(gid);
     } else {
-        uid = os::users::get_effective_uid();
-        gid = os::users::get_effective_gid().unwrap(); // // TODO (CM):
-        // I know, it's bad
-    }
-    cmd.stdin(Stdio::null())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .uid(uid)
-        .gid(gid);
-    for (key, val) in pkg.env.iter() {
-        cmd.env(key, val);
+        debug!("Cannot SETUID/SETGID; running as supervisor user instead of SVC_USER");
     }
 
-    let res = cmd.spawn();
-    //    debug!(">>>>>>> res = {:?}", res);
-
-    Ok(res?)
+    Ok(cmd.spawn()?)
 }
